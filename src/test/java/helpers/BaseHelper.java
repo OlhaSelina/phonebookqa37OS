613a15608963eeb38dcbd7d9b6b687987089e677
OlhaSelina <49640672+OlhaSelina@users.noreply.github.com>
2024-04-26 13:48:48 +0300

    update loger and screenshot

--- src/test/java/helpers/BaseHelper.java
+++ src/test/java/helpers/BaseHelper.java
@@ -3,6 +3,9 @@ package helpers;
 import com.google.common.io.Files;
 import config.ApplicationManager;
 import org.openqa.selenium.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tests.BaseTest;
 
 import java.io.File;
 
@@ -11,6 +14,7 @@ import java.util.List;
 
 public class BaseHelper {
 WebDriver driver;
+    static Logger logger = LoggerFactory.getLogger(BaseHelper.class);
 
     public BaseHelper(WebDriver driver) {
         this.driver = driver;
@@ -19,7 +23,7 @@ WebDriver driver;
     // find elemnt / elements-----------
 
     protected WebElement findElementBase(By by) {
-        System.out.println("Locator: findElement: " + by.toString());
+        logger.info("Locator: findElement: " + by.toString());
         return driver.findElement(by);
     }
 
@@ -39,7 +43,10 @@ WebDriver driver;
     // text
 
     protected String getTextBaseByLocator(By by) {
-        return findElementBase(by).getText().trim();
+
+        String text = findElementBase(by).getText().trim();
+        logger.info("get text from element: " + text);
+        return text;
     }
 
     protected String getTextBaseByElement(WebElement el) {
@@ -74,6 +81,7 @@ WebDriver driver;
     }
 
     public  String takeScreenshot(){
+        logger.info("start screenshot");
         File tmp  = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         File screenshot = new File("screenshots/screen" + System.currentTimeMillis()
         +".png");
